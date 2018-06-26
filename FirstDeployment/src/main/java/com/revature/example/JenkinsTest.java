bcba784524dfa0877df97eeac7133cd18f59eb34
Robert Crawford <RobertDev@Roberts-MacBook-Air.local>
2018-06-26 14:48:28 -0400

    Adding working code

---
+++ FirstDeployment/src/main/java/com/revature/example/JenkinsTest.java
@@ -0,0 +1,8 @@
+package com.revature.example;
+
+public class JenkinsTest {
+	public String testJenkins() {
+		String message = "Hello, Jenkins!";
+		return message;
+	}
+}
---
+++ FirstDeployment/src/test/java/com/revature/example/JenkinsTestTest.java
@@ -0,0 +1,30 @@
+package com.revature.example;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class JenkinsTestTest {
+	
+	JenkinsTest sample = new JenkinsTest();
+	
+	@Test
+	public void testAnEmptyString() {
+		assertEquals("Hello, Jenkins!", sample.testJenkins());
+	}
+}
