9ffc2ef85e19a4094b54417ec51f0fd7ec7f59a2
Robert Crawford <RobertDev@Roberts-MacBook-Air.local>
2018-06-27 11:06:33 -0400

    Ready for first deployment

--- FirstDeployment/src/test/java/com/revature/example/JenkinsTestTest.java
+++ FirstDeployment/src/test/java/com/revature/example/JenkinsTestTest.java
@@ -1,23 +1,8 @@
 package com.revature.example;
 
-import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.Month;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 public class JenkinsTestTest {
 	
